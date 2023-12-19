8c1a75b6578ec90f3da4f4a207fd44ef341509b6
HShagroon <haneenshagroon@gmail.com>
2023-12-19 18:53:57 +0300

    Initial Commit

---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Haneen Rida Shagroon on 06/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
